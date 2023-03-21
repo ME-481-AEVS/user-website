3f66ed8cbe8575911b69208c357deb93974d2722
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-03-20 20:13:22 -1000

    Add index.html

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
+// init app
 const app = express();
 
+// load views
+app.use(express.static(__dirname + '/views'));
+
+// home route
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendfile('index.html');
 });
 
 
+// start server
 app.listen(3000, (req, res) => {
   console.log('Server started on port 3000.');
 });
